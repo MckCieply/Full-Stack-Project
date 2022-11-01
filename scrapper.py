68a6a4f3265ae79e004fb5a152faf275f73242b8
Alex <mwppl123@gmail.com>
2022-11-01 21:58:28 +0100

    Finding Last Page

--- scrapper.py
+++ scrapper.py
@@ -11,5 +11,11 @@ URL = f"https://www.otomoto.pl/osobowe/{brand}/{model}"
 request = requests.get(URL)
 soup = BeautifulSoup(request.content, 'html5lib')
 offer_links = []
-for div in soup.find_all('div',attrs={'class' : "ooa-le0vtj e1b25f6f14"}):
-    print(div.a["href"])
+# for div in soup.find_all('div',attrs={'class' : "ooa-le0vtj e1b25f6f14"}):
+#     print(div.a["href"])
+div = soup.find('div', attrs = {'class':'ooa-1oll9pn e19uumca7'})
+ul = soup.find('ul', attrs={'data-testid': 'pagination-list'})
+for li in ul.find_all('li', attrs={'data-testid': 'pagination-list-item'}):
+    all_pages = int(li.a.span.text)
+    
+print(all_pages)
