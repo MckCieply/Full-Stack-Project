4b29477b6d657b160f9c166ffea592f8c028d02f
Alex <mwppl123@gmail.com>
2022-11-01 23:35:02 +0100

    single deal price

--- scrapper.py
+++ scrapper.py
@@ -21,18 +21,21 @@ def finding_last_page(brand, model, min_year, fuel_type, min_capacity):
 def all_deals(brand, model, min_year, fuel_type, min_capacity, last_page):        
     counter = 1
     for page in range(1, last_page+1):
+        deals_counter = 0
         URL = f'https://www.otomoto.pl/osobowe/{brand}/{model}/od-{min_year}?search%5Bfilter_enum_fuel_type%5D={fuel_type}&search%5Bfilter_float_engine_capacity%3Afrom%5D={min_capacity}%3Fpage%3D1"&page={page}'
         request = requests.get(URL)
         soup = BeautifulSoup(request.content, 'html5lib')
         for div in soup.find_all('div',attrs={'class' : "ooa-le0vtj e1b25f6f14"}):
-            print(counter,".",div.a["href"])
-            counter +=1
+            deals_counter +=1
+            deal_url = div.a["href"]
+            print(counter,".",deal_url)
+            single_deal(deal_url)
 
+            counter +=1
+def single_deal(deal_url):
+    request = requests.get(deal_url)
+    soup = BeautifulSoup(request.content, 'html5lib')
+    price_span = soup.find('span', {"class":"offer-price__number"})
+    print(price_span.contents[0].strip(), price_span.contents[1].text)
 finding_last_page(brand, model, min_year, fuel_type, min_capacity)
 all_deals(brand, model, min_year, fuel_type, min_capacity, last_page)
-# div = soup.find('div', attrs = {'class':'ooa-1oll9pn e19uumca7'})
-# ul = soup.find('ul', attrs={'data-testid': 'pagination-list'})
-# for li in ul.find_all('li', attrs={'data-testid': 'pagination-list-item'}):
-#     all_pages = int(li.a.span.text)
-    
-# print(all_pages)
