7fdb432b8698041ef9a22fbd0d137639bce9389e
Alex <mwppl123@gmail.com>
2022-11-03 16:03:03 +0100

    class

--- otofeature/cardeals/models.py
+++ otofeature/cardeals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from scrapper import all_deals as deals
 
 # Create your models here.
+
+class RoccoDeal(models.Model):
+
+    price = deals.price
+    currency = deals.currency
+    year = deals.year
+    mileage = deals.mileage
+    add_date = deals.add_date
