e1e1ee55eae95c978ec5dc64ee60e1fe88d462f4
Alex <mwppl123@gmail.com>
2022-11-06 19:46:42 +0100

    avg plotting

--- graf_plot.py
+++ graf_plot.py
@@ -1,5 +1,6 @@
 #plotting grafs year / avg price  AND  avg mileage / price
 import sqlite3 as sql
+import matplotlib.pyplot as plt
 
 conn = sql.connect("deals.db")
 c = conn.cursor()
@@ -7,10 +8,17 @@ c = conn.cursor()
 c.execute("SELECT price, year, mileage FROM cars")
 result = c.fetchall()
 price, year, mileage = [], [], []
-for _ in result:
-    price.append(int(_[0].replace(".", "")))
-    year.append(_[1])
-    mileage.append(int(_[2].replace(" ", "").strip("km")))
-print (mileage)
+for row in result:
+    price.append(int(row[0].replace(".", "")))
+    year.append(row[1])
+    mileage.append(int(row[2].replace(" ", "").strip("km")))
+
+#Gotta get AVG smhow dunno how yet
 
 conn.close()
+
+
+# print(max_year, min_year)
+# plt.plot(price, year)
+# plt.show()
+
