c96f050cf598adffabcea63b4d930acb618647e7
Alex <mwppl123@gmail.com>
2022-11-06 19:27:42 +0100

    Data extraction

--- graf_plot.py
+++ graf_plot.py
@@ -4,6 +4,13 @@ import sqlite3 as sql
 conn = sql.connect("deals.db")
 c = conn.cursor()
 
-
+c.execute("SELECT price, year, mileage FROM cars")
+result = c.fetchall()
+price, year, mileage = [], [], []
+for _ in result:
+    price.append(int(_[0].replace(".", "")))
+    year.append(_[1])
+    mileage.append(int(_[2].replace(" ", "").strip("km")))
+print (mileage)
 
 conn.close()
