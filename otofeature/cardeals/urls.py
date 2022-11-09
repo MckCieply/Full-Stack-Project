f861170f3479f90ba60389e86ad28c276065a43e
Alex <mwppl123@gmail.com>
2022-11-09 20:13:31 +0100

    Remove base template import to run html

--- otofeature/cardeals/urls.py
+++ otofeature/cardeals/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/roccodeals/', views.roccodeals, name='roccodeals'),
-    path('/lancerdeals/', views.lancerdeals, name='lancerdeals'),
+    path('roccodeals/', views.roccodeals, name='roccodeals'),
+    path('lancerdeals/', views.lancerdeals, name='lancerdeals'),
 ]
--- otofeature/cardeals/views.py
+++ otofeature/cardeals/views.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse
 from .models import Cars
 # Create your views here.
 
-def index(response):
-    return render(response, "cardeals/base.html", {})
+#def index(response):
+#    return render(response, "cardeals/base.html", {})
 
 def home(response):
-    return render(response, "cardeals/home.html", {})
+    return render(response, "cardeals/home.html")
 
 def roccodeals(response):
     return render(response, "cardeals/rocco.html", {})
 
 def lancerdeals(response):
     return render(response, "cardeals/lancer.html", {})
+
+#{% extends 'cardeals/base.html' %}
+#to extend base template
