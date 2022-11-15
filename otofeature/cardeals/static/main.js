61b78c645106799c0730fb46d58ddf1cfb7c209d
Alex <mwppl123@gmail.com>
2022-11-15 11:28:28 +0100

    Initializing and connecting JavaScript

---
+++ otofeature/cardeals/static/main.js
@@ -0,0 +1,5 @@
+/* Gotta create custom colors for car prices and mileage where:
+Prices: <30k - dark green, 30-40k - light green, 40-50k - yellow, >50k red
+Mileage <100k - dark green, 100-150k - light green, 150-200k yellow, >200k red
+
+Custom slider */
