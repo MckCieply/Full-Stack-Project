975d9b206960b2a655fa8702cf33445add861cca
Alex <mwppl123@gmail.com>
2022-11-17 12:51:01 +0100

    Big Class based change

--- scrapper.py
+++ scrapper.py
@@ -4,76 +4,86 @@
 #store it in DB
 import requests
 from bs4 import BeautifulSoup
-import time
 import sqlite3 as sql
-conn = sql.connect("C:/Users/mwppl/Desktop/Code/Full-Stack-Project/otofeature/deals.db")
-c = conn.cursor()
-try:    
-    c.execute(f"""CREATE TABLE cars(id INT PRIMARY KEY, brand TEXT, model TEXT, price INT, currency TEXT, year INT, mileage INT, add_date TEXT, url TEXT)""")
-except:
-    pass
 
-brand = "Mitsubishi"    #Mitsubishi Volkswagen
-model = "Lancer"  #Lancer Scirocco
-min_year = "2007" #2007 2008
-fuel_type = "petrol"#petrol
-min_capacity = "1700"#1900 1500-1700
-chasis = "sedan" #sedan coupe
-def finding_last_page(brand, model, min_year, fuel_type, min_capacity):
-    URL = f"https://www.otomoto.pl/osobowe/{brand}/{model}/seg-{chasis}/od-{min_year}?search%5Bfilter_enum_fuel_type%5D={fuel_type}&search%5Bfilter_float_engine_capacity%3Afrom%5D={min_capacity}?page=1"
-    request = requests.get(URL)
-    soup = BeautifulSoup(request.content, 'html5lib')
-    try:
-        div = soup.find('div', {'class':'ooa-1oll9pn e19uumca7'})
-        ul = div.find('ul', {'data-testid': 'pagination-list'})
-        for li in ul.find_all('li', {'data-testid': 'pagination-list-item'}):
-            global last_page
-            last_page = int(li.a.span.text)
-    except:
-        last_page = 1
-        
-def all_deals(brand, model, min_year, fuel_type, min_capacity, last_page):        
-    counter = 1
-    for page in range(1, last_page+1):
-        URL = f'https://www.otomoto.pl/osobowe/{brand}/{model}/seg-{chasis}/od-{min_year}?search%5Bfilter_enum_fuel_type%5D={fuel_type}&search%5Bfilter_float_engine_capacity%3Afrom%5D={min_capacity}%3Fpage%3D1"&page={page}'
+class Car():
+    def __init__(self, brand, model, min_year, fuel_type, min_capacity, chasis):
+        self.brand = brand
+        self.model = model 
+        self.min_year = min_year 
+        self.fuel_type = fuel_type
+        self.min_capacity = min_capacity
+        self.chasis = chasis
+        self.finding_last_page()
+        self.all_deals()
+        self.db_query()
+
+    def finding_last_page(self):
+        URL = f"https://www.otomoto.pl/osobowe/{self.brand}/{self.model}/seg-{self.chasis}/od-{self.min_year}?search%5Bfilter_enum_fuel_type%5D={self.fuel_type}&search%5Bfilter_float_engine_capacity%3Afrom%5D={self.min_capacity}?page=1"
         request = requests.get(URL)
         soup = BeautifulSoup(request.content, 'html5lib')
-        for div in soup.find_all('div',attrs={'class' : "ooa-le0vtj e1b25f6f14"}):
-            deal_url = div.a["href"]
-            #print(counter,".",deal_url)
-            single_deal(deal_url)
+        try:
+            div = soup.find('div', {'class':'ooa-1oll9pn e19uumca7'})
+            ul = div.find('ul', {'data-testid': 'pagination-list'})
+            for li in ul.find_all('li', {'data-testid': 'pagination-list-item'}):
+                global last_page
+                last_page = int(li.a.span.text)
+        except:
+            last_page = 1
+        print("Last page: ", last_page)
+        print("URL: ", URL)
+    def all_deals(self):        
+        counter = 1
+        for page in range(1, last_page+1):
+            URL = f'https://www.otomoto.pl/osobowe/{self.brand}/{self.model}/seg-{self.chasis}/od-{self.min_year}?search%5Bfilter_enum_fuel_type%5D={self.fuel_type}&search%5Bfilter_float_engine_capacity%3Afrom%5D={self.min_capacity}%3Fpage%3D1"&page={page}'
+            request = requests.get(URL)
+            soup = BeautifulSoup(request.content, 'html5lib')
+            for div in soup.find_all('div',attrs={'class' : "ooa-le0vtj e1b25f6f14"}):
+                self.deal_url = div.a["href"]
+                #print(counter,".",deal_url)
+                self.single_deal(self.deal_url)
 
-            counter +=1
-        print(f"Total of {counter} deals")
-def single_deal(deal_url):                      #extract id/ deal add date/ price/ production year/ mileage
-    request = requests.get(deal_url)
-    soup = BeautifulSoup(request.content, 'html5lib')
-    span = soup.find('span', {"class":"offer-price__number"})
-    price = span.contents[0].replace(" ", "")
-    currency = span.contents[1].text
+                counter +=1
+            print(f"Total of {counter} deals")
+    def single_deal(self,deal_url):                      #extract id/ deal add date/ price/ production year/ mileage
+        print("Single deal URL: ", deal_url)
+        request = requests.get(deal_url)
+        soup = BeautifulSoup(request.content, 'html5lib')
+        span = soup.find('span', {"class":"offer-price__number"})
+        self.price = span.contents[0].replace(" ", "")
+        self.currency = span.contents[1].text
 
-    span = soup.find_all('span', {'class':'offer-main-params__item'})
-    year = span[0].text.strip()
-    mileage = span[1].text.strip().replace(" ", "").strip("km")
+        span = soup.find_all('span', {'class':'offer-main-params__item'})
+        self.year = span[0].text.strip()
+        self.mileage = span[1].text.strip().replace(" ", "").strip("km")
 
-    span = soup.find('span',{'class':'offer-meta__value'})
-    add_date = span.contents[0]
+        span = soup.find('span',{'class':'offer-meta__value'})
+        self.add_date = span.contents[0]
 
-    span = soup.find('span',{'id':'ad_id'})
-    id = span.text
+        span = soup.find('span',{'id':'ad_id'})
+        self.id = span.text
+    def db_query(self):
+        conn = sql.connect("C:/Users/mwppl/Desktop/Code/Full-Stack-Project/otofeature/deals.db")
+        c = conn.cursor()
+        try:    
+            c.execute(f"""CREATE TABLE cars(id INT PRIMARY KEY, brand TEXT, model TEXT, price INT, currency TEXT, year INT, mileage INT, add_date TEXT, url TEXT)""")
+        except:
+            pass
+        try:
+            c.execute("""INSERT INTO  cars VALUES (?,?,?,?,?,?,?,?,?)""",(self.id, self.brand, self.model, self.price, self.currency, self.year, self.mileage, self.add_date, self.deal_url))
+        except:
+            pass
+        conn.commit()
 
-    #print(f"{id}, {brand}, {model}, {price}, {currency}, {year}, {mileage}, {add_date}")
-    try:
-        c.execute("""INSERT INTO  cars VALUES (?,?,?,?,?,?,?,?,?)""",(id, brand, model, price, currency, year, mileage, add_date, deal_url))
-    except:
-        pass
-    
-if __name__ == "__main__":
-    finding_last_page(brand, model, min_year, fuel_type, min_capacity)
-    all_deals(brand, model, min_year, fuel_type, min_capacity, last_page)
+        print(f"Finishing... ")
+            
+        conn.close()
 
-conn.commit()
+scirocco = Car("Volkswagen", "Scirocco", "2008", "petrol", "1900", "")
+lancer = Car("Mitsubishi", "Lancer", "2007", "petrol", "1700", "sedan")
 
-print(f"Finishing... ")
-    
-conn.close()
+
+# if __name__ == "__main__":
+#     scirocco.finding_last_page()
+#     scirocco.all_deals()
+
