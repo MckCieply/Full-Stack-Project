e4866c3f4bdbbcaac69dec84d8b98fa459b42a6e
Alex <mwppl123@gmail.com>
2022-11-21 00:40:15 +0100

    sql query

--- mileage_plot.py
+++ mileage_plot.py
@@ -1,2 +1,11 @@
 #Plotting avg mileage for each year of production, for each of models
 #and paste it on homepage
+import sqlite3 as sql
+conn = sql.connect("C:/Users/mwppl/Desktop/Code/Full-Stack-Project/otofeature/deals.db")
+c = conn.cursor()
+
+c.execute("SELECT ROUND(avg(mileage)), model, year from Cars group by model, year order by model, year")
+rows = c.fetchall()
+
+for row in rows:
+    print(row)
