390ba713609e01969646466fcae9c9d094a4ebf6
Alex <mwppl123@gmail.com>
2022-11-20 13:01:34 +0100

    sql query

--- price_plot.py
+++ price_plot.py
@@ -1,2 +1,13 @@
 #Plotting avg price for each year of production, for each of models
 #paste plot on home page
+import sqlite3 as sql
+
+conn = sql.connect("C:/Users/mwppl/Desktop/Code/Full-Stack-Project/otofeature/deals.db")
+c = conn.cursor()
+
+c.execute("SELECT ROUND(avg(price)), model, year from Cars group by model, year order by model, year")
+
+rows = c.fetchall()
+
+for row in rows:
+    print(row)
