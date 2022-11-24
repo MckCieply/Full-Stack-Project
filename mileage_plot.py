4c3b9daef1640e08be437513b73be3a2ad9325ff
Alex <mwppl123@gmail.com>
2022-11-24 01:27:46 +0100

    Plotting customization

--- mileage_plot.py
+++ mileage_plot.py
@@ -10,7 +10,7 @@ c = conn.cursor()
 c.execute("SELECT ROUND(avg(mileage)), model, year from Cars group by model, year order by model, year")
 rows = c.fetchall()
 
-fix, ax = plt.subplots()
+fig, ax = plt.subplots()
 avg_mileage = []
 year = []
 for row in rows:
@@ -18,7 +18,7 @@ for row in rows:
         avg_mileage.append(row[0])
         year.append(row[2])
 
-ax.plot(year, avg_mileage, label = "Scirocco", linewidth = 2, marker =".", color="m" )
+ax.plot(year, avg_mileage, label = "Scirocco", linewidth = 2, marker =".", color="magenta" )
 
 avg_mileage = []
 year = []
@@ -26,7 +26,7 @@ for row in rows:
     if(row[1] == "Lancer"):
         avg_mileage.append(row[0])
         year.append(row[2])
-ax.plot(year, avg_mileage, label = "Lancer", linewidth = 2, marker =".", color="red")
+ax.plot(year, avg_mileage, label = "Lancer", linewidth = 2, marker =".", color="white")
 
 avg_mileage = []
 year = []
@@ -34,16 +34,32 @@ for row in rows:
     if(row[1] == "c30"):
         avg_mileage.append(row[0])
         year.append(row[2])
-ax.plot(year, avg_mileage, label = "c30", linewidth = 2, marker =".", color="lightblue")
+ax.plot(year, avg_mileage, label = "c30", linewidth = 2, marker =".", color="black")
 
-ax.set_title("Average prices")                                              
+titleparams = {
+'fontsize': 20,
+ 'fontweight':'bold',
+ 'color': 'white',
+ 'verticalalignment': 'baseline',
+ 'horizontalalignment': 'center'}
+ax.set_title("Average mileage", color="white", fontdict=titleparams)
+                                              
 ax.legend()
-ax.set_xlabel("Year")
-ax.set_ylabel("Avg. Mileage")
+ax.set_xlabel("Year", color="white")
+ax.set_ylabel("Avg. Mileage", color="white")
 ax.yaxis.set_major_locator(ML(50000))
 majors = ['50k','100k','150k','200k','250k','300k','350k','400k']
 ax.yaxis.set_major_formatter(ticker.FixedFormatter(majors))
 ax.xaxis.set_major_locator(ML(1))
-ax.grid(True, linestyle="--")
-# plt.savefig("otofeature\cardeals\static\MileagePlot.png")
-plt.show()
+ax.yaxis.set_minor_locator(ML(25000))
+ax.grid(True, linestyle="--", color="#00607a" )
+ax.set_facecolor("#002029")
+fig.set_facecolor('#00303d')
+
+ax.spines['left'].set_color('white')
+ax.spines['bottom'].set_color('white')
+ax.tick_params(which="both", colors="white")
+ax.tick_params(which="minor", length = 5)
+ax.tick_params(which="major", length = 8)
+plt.savefig("otofeature\cardeals\static\MileagePlot.png")
+#plt.show()
--- price_plot.py
+++ price_plot.py
@@ -24,29 +24,45 @@ for row in rows:
     if row[1] == "Scirocco":
         avg_prices.append(row[0])
         year.append(row[2])
-ax.plot(year, avg_prices, label = "Scirocco", linewidth = 2, marker = ".")
+ax.plot(year, avg_prices, label = "Scirocco", linewidth = 2, marker = ".", color="magenta")
 avg_prices = []
 year = []
 for row in rows:
     if row[1] == "Lancer":
         avg_prices.append(row[0])
         year.append(row[2])
-ax.plot(year, avg_prices, label = "Lancer", linewidth = 2, marker = ".")
+ax.plot(year, avg_prices, label = "Lancer", linewidth = 2, marker = ".", color="white")
 avg_prices = []
 year = []
 for row in rows:
     if row[1] == "c30":
         avg_prices.append(row[0])
         year.append(row[2])
-ax.plot(year, avg_prices, label = "c30", linewidth = 2, marker = ".")       #Plot one of lines
-ax.set_title("Average prices")                                              #Set graph title
+ax.plot(year, avg_prices, label = "c30", linewidth = 2, marker = ".", color="black")
+
+titleparams = {                                                             #Title parameters
+'fontsize': 20,
+ 'fontweight':'bold',
+ 'color': 'white',
+ 'verticalalignment': 'baseline',
+ 'horizontalalignment': 'center'}
+ax.set_title("Average prices", fontdict=titleparams, color="white")         #Set graph title
+
 ax.legend()                                                                 #Show legend
-ax.set_xlabel("Year")                                                       #Show x label
-ax.set_ylabel("Avg. Mileage")                                               #show y label
+ax.set_xlabel("Year", color="white")                                        #Show x label
+ax.set_ylabel("Avg. Mileage", color="white")                                #show y label
 ax.yaxis.set_major_locator(ML(10000))                                       #set range between marks on y
 majors = ['10k','20k','30k','40k','50k','60k','70k','80k','90k','100k']     #custom values for y
 ax.yaxis.set_major_formatter(ticker.FixedFormatter(majors))                 #use custom values for y
 ax.xaxis.set_major_locator(ML(1))                                           #set range between marks on x
-ax.grid(True, linestyle="--" )                                              #set grid 
+ax.set_facecolor("#002029")                                                 #graph background color
+fig.set_facecolor('#00303d')                                                #background behind axes
+ax.grid(True, linestyle="--", color="#00607a" )                             #set grid 
+
+ax.spines['left'].set_color('white')                                        #white x 'n' y axes
+ax.spines['bottom'].set_color('white')
+ax.tick_params(which="both", colors="white")                                #customization of ticks
+ax.tick_params(which="major", length = 8)
+
 plt.savefig("otofeature\cardeals\static\PricePlot.png")                     #save plot
-# plt.show()
+#plt.show()
